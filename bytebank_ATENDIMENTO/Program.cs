00e76016a1126180f1ba1228c2069b5b17854a68
Serpa <jhonatanmeireles12@yahoo.com.br>
2024-01-27 23:50:18 -0300

    Trabalhando com a class Array

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -1,13 +1,10 @@
-﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+﻿using System;
 
-int idade = 30;
+Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
 
-int[] idades = new int[5];
-idades[0] = 30;
-idades[1] = 40;
-idades[2] = 17;
-idades[3] = 21;
-idades[4] = 18;
+
+//TestaArrayInt();
+//TestaBuscarPalavra();
 
 void TestaArrayInt()
 {
@@ -55,5 +52,29 @@ void TestaBuscarPalavra()
     }
 }
 
-//TestaArrayInt();
-TestaBuscarPalavra();
+Array amostra = Array.CreateInstance(typeof(double), 5);
+amostra.SetValue(5.9, 0);
+amostra.SetValue(1.8, 1);
+amostra.SetValue(7.1, 2);
+amostra.SetValue(10,3);
+amostra.SetValue(6.9,4);
+TestaMediana(amostra);
+void TestaMediana(Array array)
+{
+    if((array == null) || (array.Length == 0))
+    {
+        Console.WriteLine("Array para cálculo da mediana estpa vazio ou nulo");
+    }
+    else
+    {
+        double[] numerosOrdenados = (double[]) array.Clone();
+        Array.Sort(numerosOrdenados);
+        
+        int tamanho =  numerosOrdenados.Length;
+        int meio = tamanho / 2;
+        double mediana = (tamanho % 2 != 0) ? numerosOrdenados[meio] :
+            (numerosOrdenados[meio] + numerosOrdenados[meio - 1]) / 2;
+
+        Console.WriteLine($"Com base na amostra na amostra a mediana = {mediana}.");
+    }
+}
