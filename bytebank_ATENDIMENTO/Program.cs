aaa6727552487033ce89f9af39d05575d0067f56
Serpa <jhonatanmeireles12@yahoo.com.br>
2024-02-02 00:35:35 -0300

    Testando recursos com o List

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -123,7 +123,7 @@ List<ContaCorrente> _listaContas = new List<ContaCorrente>()
     new ContaCorrente(94, "987321-W") { Saldo = 60 }
 };
 
-AtendimentoCliente();
+//AtendimentoCliente();
 void AtendimentoCliente()
 {
     char opcao = '0';
@@ -217,4 +217,44 @@ void ListarContas()
         Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
         Console.ReadKey();
     }
+}
+
+List<ContaCorrente> _listaContas2 = new List<ContaCorrente>()
+{
+    new ContaCorrente(874, "5679787-A"),
+    new ContaCorrente(874, "445668-B"),
+    new ContaCorrente(874, "7781438-C")
+};
+
+List<ContaCorrente> _listaContas3 = new List<ContaCorrente>()
+{
+    new ContaCorrente(951, "5679787-E"),
+    new ContaCorrente(321, "445668-F"),
+    new ContaCorrente(719, "7781438-G")
+};
+
+_listaContas2.AddRange(_listaContas3);
+_listaContas2.Reverse();
+
+for(int i = 0; i < _listaContas2.Count; i++)
+{
+    Console.WriteLine($"Indice[{i}] = Conta [{_listaContas2[i].Conta}]");
+}
+
+Console.WriteLine("\n\n");
+
+var range = _listaContas3.GetRange(0,1);
+
+for (int i = 0; i < range.Count; i++)
+{
+    Console.WriteLine($"Indice[{i}] = Conta [{range[i].Conta}]");
+}
+
+Console.WriteLine("\n\n");
+
+_listaContas3.Clear();
+
+for (int i = 0; i < _listaContas3.Count; i++)
+{
+    Console.WriteLine($"Indice[{i}] = Conta [{range[i].Conta}]");
 }
