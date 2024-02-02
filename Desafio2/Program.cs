f2d63270fb65401eed2fad55f60648926c77469c
Serpa <jhonatanmeireles12@yahoo.com.br>
2024-02-02 00:42:40 -0300

    Realizando o Desafio2

---
+++ Desafio2/Program.cs
@@ -0,0 +1,25 @@
+﻿List<string> nomesDosEscolhidos = new List<string>()
+{
+    "Bruce Wayne",
+    "Carlos Vilagran",
+    "Richard Grayson",
+    "Bob Kane",
+    "Will Farrel",
+    "Lois Lane",
+    "General Welling",
+    "Perla Letícia",
+    "Uxas",
+    "Diana Prince",
+    "Elisabeth Romanova",
+    "Anakin Wayne"
+};
+
+
+void ExisteONome(List<string> receba)
+{
+    if(receba.Contains("Anakin Wayne"))
+        Console.Write("Localizado");
+    Console.WriteLine("\n\n");
+}
+
+ExisteONome(nomesDosEscolhidos);
