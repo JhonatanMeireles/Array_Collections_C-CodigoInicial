318bdba52bd4b69d6e8fd3fb74fae843dad30ba8
Serpa <jhonatanmeireles12@yahoo.com.br>
2024-02-06 01:21:27 -0300

    Finalizando a aplicação melhorando o código encapsulando os métodos na Classe ByteBankAtendimento

--- bytebank_ATENDIMENTO/bytebank.Modelos/Conta/ContaCorrente.cs
+++ bytebank_ATENDIMENTO/bytebank.Modelos/Conta/ContaCorrente.cs
@@ -103,13 +103,20 @@
 			Titular = new Cliente();
 			TotalDeContasCriadas++;
 		}
+        public ContaCorrente(int numero_agencia)
+        {
+            Numero_agencia = numero_agencia;
+            Conta = Guid.NewGuid().ToString().Substring(0, 8);
+            Titular = new Cliente();
+            TotalDeContasCriadas++;
+        }
 
-		public override string ToString()
+        public override string ToString()
 		{
 
 			return $" === DADOS DA CONTA === \n" +
 				   $"Número da Conta : {this.Conta} \n" +
-				   $"Número da Agência: {this.Numero_agencia}" +
+				   $"Número da Agência: {this.Numero_agencia} \n" +
 				   $"Titular da Conta: {this.Titular.Nome} \n" +
 				   $"CPF do Titular  : {this.Titular.Cpf} \n" +
 				   $"Profissão do Titular: { this.Titular.Profissao}" +
